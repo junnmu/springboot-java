cc48d21e1ba1010af34f22f5cd428ef306838664
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2020-12-07 15:30:17 -0300

    Service layer, component registration

--- src/main/java/com/jun/spring/repositories/UserRepository.java
+++ src/main/java/com/jun/spring/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.jun.spring.repositories;
 import com.jun.spring.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Não preciso da anotação de que é um @Repository, pois já estou herdando de um!
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/jun/spring/services/UserService.java
@@ -0,0 +1,26 @@
+package com.jun.spring.services;
+
+import com.jun.spring.entities.User;
+import com.jun.spring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+// Registrando que minha classe é um componente do String, especialmente um serviço
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
